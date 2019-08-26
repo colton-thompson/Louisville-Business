01bbba95f81b1e26d7b826f29d9e62e6b86b05df
Colton Thompson <thompson.coltonj@gmail.com>
2019-08-25 23:25:21 -0400

    start of adding inspection scores

--- business_mapper.py
+++ business_mapper.py
@@ -1,8 +1,10 @@
 import folium
 import pandas
 
-df = pandas.read_csv("businesses.csv", nrows = 100)
-	
+df_b = pandas.read_csv("businesses.csv")#, nrows = 100)
+df_b = df_b.sort_values("business_id", ascending = False)
+print(df_b.head(n=10))
+
 map = folium.Map(location=[38.217090,-85.742117],zoom_start= 13)
 #map = folium.Map(location=[df['latitude'].mean(),df['longitude'].mean()],zoom_start= 13)
 
@@ -11,9 +13,9 @@ def getPopupInfo(name, address):
 	return popup
 	
 fg = folium.FeatureGroup(name="Restuarants")
-for la,lo,name,addy in zip(df["latitude"],df["longitude"],df["name"], df["address"]):
+for la,lo,name,addy in zip(df_b["latitude"],df_b["longitude"],df_b["name"], df_b["address"]):
 	#scooter_fg.add_child(folium.CircleMarker(location=[la,lo], radius = 5, color = 'black', weight = 4, popup = "Scooter: " + bikeID))
-	fg.add_child(folium.Marker(location=[la,lo],popup=(folium.Popup(getPopupInfo(name, addy))),icon=folium.Icon(color="red",icon_color='white')))
+	fg.add_child(folium.Marker(location=[la,lo],popup=(folium.Popup(getPopupInfo(name, addy))),icon=folium.Icon(color="red", icon_color='white')))
 
 map.add_child(fg)
 
