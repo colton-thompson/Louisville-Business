0cf000531abd02592adaf7385633d6f37b1d1c70
Colton Thompson <thompson.coltonj@gmail.com>
2019-08-29 14:55:04 -0400

    filtering data correctly now

--- business_mapper.py
+++ business_mapper.py
@@ -1,19 +1,11 @@
+import sys
+import csv
+import pprint
 import folium
 import pandas
 
-df_b = pandas.read_csv("businesses.csv", nrows = 10)
-df_i = pandas.read_csv("inspections.csv")
-
-
-#filter data
-df_b = df_b[df_b['latitude']!=0.0]
-df_b = df_b[df_b['longitude']!=0.0]
-df_b = df_b[df_b.phone_number.notnull()]
-df_b = df_b.sort_values("business_id", ascending = False)
-print(df_b.head(n=10))
-
-newDateList = []
 def reformatDate(df):
+	newDateList = []
 	for dateIndex in df["date"]:
 		year = str(dateIndex)
 		year = (year[0:4])
@@ -28,44 +20,48 @@ def reformatDate(df):
 		newDateList.append(newDate)
 	return newDateList
 
-df_i = df_i[df_i['score']!=0]
-#df_i = df_i['type'].str.contains("routine")
+#read in file, return dictionary
+def creat_Dict_from_CSV(f):
+	reader = csv.DictReader(open(f, 'r',))
+	dict_list = []
+	for line in reader:
+		dict_list.append(line)
+	return dict_list
+	
+def getPopupInfo(name, address):
+	#score = attachScore()
+	popup = name + "<br>Address: " + addy +"<br>Score: " + str(-1)
+	return popup
 
+#read files
+df_b = pandas.read_csv("businesses.csv", nrows = 10)
+df_i = pandas.read_csv("inspections.csv")
+
+#filter business data
+df_b = df_b[df_b['latitude']!=0.0]
+df_b = df_b[df_b['longitude']!=0.0]
+#df_b = df_b[df_b.phone_number.notnull()]
+df_b = df_b.sort_values("business_id", ascending = True)
+print(df_b.head(n=10))
+
+#filter inspection data
+df_i = df_i[df_i['score']!=0]
 df_i["date"] = reformatDate(df_i)
-df_i = df_i.sort_values("business_id", ascending = False)
+df_i = df_i.sort_values("date", ascending = False)
 print(df_i.head(n=10))
 
+#create dictionaries
+businessDict = creat_Dict_from_CSV("businesses.csv")
+inspectionDict = creat_Dict_from_CSV("inspections.csv")
+
+print('')
+print(businessDict[0]['business_id'])
+print(inspectionDict[0]['business_id'])
+
+
+#Folium Mapping Section
 map = folium.Map(location=[38.217090,-85.742117],zoom_start= 13)
 #map = folium.Map(location=[df['latitude'].mean(),df['longitude'].mean()],zoom_start= 13)
-
-
-
-def getPopupInfo(name, address):
-	score = attachScore()
-	popup = name + "<br>Address: " + addy +"<br>Score: " + str(score)
-	return popup
-	
-#SCRAPE THIS IDEA
-'''
-business_id_List = []
-def attachScore():
-	for index in df_i['business_id']:
-		print(index)
-		if (index not in business_id_List):
-			print("not in")
-			for index2 in df_b['business_id']:
-				if (index == index2):
-					print("match")	
-					score = df_i['score']
-					business_id_List.append(index)
-					return score
-		else:
-			print("breaking")
-			return -1
-			'''
-#TODO
-#NEW IDEA: create a dictionary and map business id to respective values such as name and date of inspection
-#		   when displaying information, filter high to low, then display top result 
 
 fg = folium.FeatureGroup(name="Restuarants")
 for la,lo,name,addy in zip(df_b["latitude"],df_b["longitude"],df_b["name"], df_b["address"]):
