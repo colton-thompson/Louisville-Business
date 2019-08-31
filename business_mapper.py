aa33b6c578c2379cd243ac1f5ab15d4960f4e565
Colton Thompson <thompson.coltonj@gmail.com>
2019-08-31 01:38:46 -0400

    filter DFs correctly now, then using output as input for dictionaries

--- business_mapper.py
+++ business_mapper.py
@@ -34,40 +34,49 @@ def getPopupInfo(name, address):
 	return popup
 	
 def assign_Inspection_Score():
-	dict_list = []
+	dict_list = {}
 	for bus_index in range(len(businessDict)):
 		for insp_index in range(len(inspectionDict)):
 			if (businessDict[bus_index]['business_id'] == inspectionDict[insp_index]['business_id']):
 				print("match found at " + str(businessDict[bus_index]['business_id']) + "\tScore: " + inspectionDict[insp_index]['score'] + "\tDate: " + inspectionDict[insp_index]['date'] )
 				
-		
-
-#read files
-df_b = pandas.read_csv("businesses.csv", nrows = 10)
+def filterDataFile_Businesses(df_b):
+	df_b = df_b[df_b['latitude']!=0.0]
+	df_b = df_b[df_b['longitude']!=0.0]
+	#df_b = df_b[df_b.phone_number.notnull()]
+	df_b = df_b.sort_values("business_id", ascending = True)
+	#print(df_b.head(n=10))
+	df_b = pandas.DataFrame({'business_id': df_b["business_id"],'name': df_b["name"],'address': df_b["address"], 'city':df_b["city"], 'state':df_b["state"], 'postal_code':df_b["postal_code"], 'latitude':df_b["latitude"],'longitude':df_b["longitude"], 'phone_number':df_b["phone_number"]}).to_csv('bus_test.csv')
+	
+def filterDataFile_Inspections(df_i):
+	df_i = df_i[df_i['date'] > 20160000]
+	df_i = df_i.dropna(subset = ['score']) #filter out scores of 0, nan, ect.
+	df_i = df_i.sort_values("date", ascending = False)
+	#print(df_i.head(n=1000))
+	df_i = pandas.DataFrame({'business_id': df_i["business_id"],'score': df_i["score"],'date': df_i["date"], 'description':df_i["description"], 'type':df_i["type"]}).to_csv('insp_test.csv')
+				
+				
+# check if files are up to date otherwise update them
+# run this section first and save the filtered data as a new csv
+# read files
+df_b = pandas.read_csv("businesses.csv")#, nrows = 10)
 df_i = pandas.read_csv("inspections.csv")
 
-#filter business data
-df_b = df_b[df_b['latitude']!=0.0]
-df_b = df_b[df_b['longitude']!=0.0]
-#df_b = df_b[df_b.phone_number.notnull()]
-df_b = df_b.sort_values("business_id", ascending = True)
-print(df_b.head(n=10))
-
-#filter inspection data
-df_i = df_i[df_i['score']!=0]
-df_i["date"] = reformatDate(df_i)
-df_i = df_i.sort_values("date", ascending = False)
-print(df_i.head(n=10))
+#filter data
+filterDataFile_Businesses(df_b)
+#df_i["date"] = reformatDate(df_i)
+filterDataFile_Inspections(df_i)
 
 #create dictionaries
-businessDict = creat_Dict_from_CSV("businesses.csv")
-inspectionDict = creat_Dict_from_CSV("inspections.csv")
+
+businessDict = creat_Dict_from_CSV("bus_test.csv")
+inspectionDict = creat_Dict_from_CSV("insp_test.csv")
 
 mergedDict = assign_Inspection_Score()
 
-print('')
 print(businessDict[0]['business_id'])
 print(inspectionDict[0]['business_id'])
+
 
 #Folium Mapping Section
 map = folium.Map(location=[38.217090,-85.742117],zoom_start= 13)
@@ -75,13 +84,12 @@ map = folium.Map(location=[38.217090,-85.742117],zoom_start= 13)
 
 fg = folium.FeatureGroup(name="Restuarants")
 for la,lo,name,addy in zip(df_b["latitude"],df_b["longitude"],df_b["name"], df_b["address"]):
+#for la,lo,name,addy in zip(businessDict["latitude"],businessDict["longitude"],businessDict["name"], businessDict["address"]):
 	try:
 		fg.add_child(folium.Marker(location=[la,lo], popup=(folium.Popup(getPopupInfo(name, addy),max_width=150,min_height=200)), icon=folium.Icon(color="red", icon_color='white')))
 	except TypeError:
 		raise
-		
-#df_b = pandas.DataFrame({'business_id': df_b["business_id"],'name': df_b["name"],'address': df_b["address"], 'city':df_b["city"], 'state':df_b["state"], 'postal_code':df_b["postal_code"], 'latitude':df_b["latitude"],'longitude':df_b["longitude"], 'phone_number':df_b["phone_number"]}).to_csv('test.csv')
-
+	
 map.add_child(fg)
 
 map.add_child(folium.LayerControl())
